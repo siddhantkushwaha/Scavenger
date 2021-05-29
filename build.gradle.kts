04eb99cf4e808d79af98ea53efb2412f5b66fc77
siddhantkushwaha <k16.siddhant@gmail.com>
2021-05-29 21:51:37 +0530

    specify main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.siddhantkushwaha.scavenger.ScavengerApplicationKt"
+    }
 }
